7531487babe561f8f9a71e533e3d0fcb74f51002
Dimitar85898 <83722607+Dimitar85898@users.noreply.github.com>
2023-08-31 19:03:07 +0300

    0.2.0A Added Page mapping

--- src/BukoOS/kernel.cpp
+++ src/BukoOS/kernel.cpp
@@ -4,9 +4,10 @@
 #include "libs/memory.h"
 #include "libs/display.h"
 #include "libs/terminal.h"
-#define BUKO_PRINT_MEM_INFO
+//#define BUKO_PRINT_MEM_INFO
+//#define BUKO_TEST_MEMORY_PAGE_MAP
 #define SYS_DIST_URL "https://github.com/Dcraftbg/BukoOS"
-#define SYS_VERSION "0.1.0A"
+#define SYS_VERSION "0.2.0A"
 #ifdef BUKO_DEBUG
    #define SYS_MODE "Debug"
 #elif defined(BUKO_RELEASE)
@@ -17,9 +18,6 @@
    #error "Running in non recognized more! Please run with BUKO_DEBUG, BUKO_RELEASE or BUKO_DIST"
 #endif
 #define MEMORY_DISPLAY_SIZE (112/2)
-// #include "libs/terminal.cpp"
-// #include "libs/memory.cpp"
-
 static volatile limine_framebuffer_request limine_framebuffer_request = {
     .id = LIMINE_FRAMEBUFFER_REQUEST,
     .revision = 0
@@ -33,7 +31,6 @@ static volatile limine_memmap_request limine_memmap_request = {
 // Consider:
 // DisplayInfo globalInfo={0};
 
-// TODO: Remove super comments and normal comments that were previously only used for testing
 #include "libs/string.h"
 /*
 #define LIMINE_MEMMAP_USABLE                 0
@@ -48,11 +45,13 @@ static volatile limine_memmap_request limine_memmap_request = {
 Pixel limine_memmap_type_to_color(uint64_t type) {
   switch(type) {
         case LIMINE_MEMMAP_USABLE:
+        case LIMINE_MEMMAP_BOOTLOADER_RECLAIMABLE:
+        case LIMINE_MEMMAP_ACPI_RECLAIMABLE:
            return Pixel(0x00,0xFF,0x00,0xFF);
         case LIMINE_MEMMAP_RESERVED:
            return Pixel(0x00,0x00,0xFF,0xFF);
            //return Pixel(0x88,0xFF,0x88,0xFF);   // light green
-        case LIMINE_MEMMAP_BOOTLOADER_RECLAIMABLE:
+
         case LIMINE_MEMMAP_FRAMEBUFFER:
            return Pixel(0x88,0x88,0x88,0xFF);
         case LIMINE_MEMMAP_BAD_MEMORY:
@@ -85,14 +84,18 @@ const char* limine_memmap_type_to_string(uint64_t type) {
    }
 }
 #include "libs/memory.h"
-
-extern "C" void kernel() {
-/*
-    uint8_t rskip = 1;
-    uint8_t bskip = 0;
-    uint8_t gskip = 0;
-*/
-    
+#define PAGE_STATUS_UNUSABLE 0
+#define PAGE_STATUS_USABLE   1
+inline void setPageStatus(void* memoryMap, size_t i, char status) {
+    char* pageSet = &((char*)memoryMap)[i/8];
+    *pageSet &= ~(1 << i%8);
+    *pageSet |= (status << i%8);
+}
+inline char getPageStatus(void* memoryMap, size_t i) {
+     char* pageSet = &((char*)memoryMap)[i/8];
+     return ((*pageSet) & (1<<i%8))>>i%8;
+}
+extern "C" void kernel() {    
 	if( limine_framebuffer_request.response == NULL || limine_framebuffer_request.response->framebuffer_count != 1 || limine_framebuffer_request.response->framebuffers[0] -> bpp != 32 )
 		for(;;);
     if( limine_memmap_request.response == NULL)
@@ -110,91 +113,133 @@ extern "C" void kernel() {
         pixels = (Pixel*)((uintptr_t)pixels +  pitch);
     }
     pixels=orgpixels;
-    
-#if 0
-    pixels[0].r = 0xFF;
-    pixels[1].r = 0xFF;          
-    pixels[2].r = 0xFF;
-    pixels = (Pixel*) ((uintptr_t) pixels + pitch);
-    pixels[0].r = 0xFF;
-    pixels = (Pixel*) ((uintptr_t) pixels + pitch);
-    pixels[0].r = 0xFF;
-    pixels[1].r = 0xFF;
-    pixels[2].r = 0xFF;
-    pixels = (Pixel*) ((uintptr_t) pixels + pitch);
-    pixels[0].r = 0xFF;
-    pixels = (Pixel*) ((uintptr_t) pixels + pitch);
-    pixels[0].r = 0xFF;
-    pixels[1].r = 0xFF;
-    pixels[2].r = 0xFF;
-    pixels = orgpixels;
-#endif
     DisplayInfo display(pixels, width, height, pitch);
-#if 0
 
-    {
-        Pixel res = {0};
-        res.a=0xFF;
-        res.r=0xFF;
-        display.setAt(1,1,res);
-    }
-#endif
-#if 0
-    Pixel res = {0};
-    res.a=0xFF;
-    res.r=0xFF;
-    putChar(display,0,0,'H',res); 
-    putChar(display,8,0,'e',res);
-    putChar(display,16,0,'l',res);
-#endif
-    //putChar(display,0,0,'H',res); 
-    Cursor_T& cursor = getGlobalCursor();
+
     volatile bool running=true;
-    //putStr(display,"\n");
-    //putStr(display,"\tHello World!\r\tI overwrote that shit :D");
+
 
     putStr(display, "Hello and welcome to BukoOS!\n");
     putStr(display, "Distribution from DcraftBg " SYS_DIST_URL "\n"); 
-    //putStr(display, "System version ");    putStr(display, SYS_VERSION);    putStr(display, "\n");
     stdTerminal::printf(display, "System version %s in %s mode\n", SYS_VERSION, SYS_MODE);
+#ifdef BUKO_PRINT_MEM_INFO
     stdTerminal::printf(display, "Number of map entries available: %d\n", (int)limine_memmap_request.response->entry_count);
+#endif
     size_t AvailableMemory=0;
     size_t totalMemory=0;
+    int64_t MaxReclaimableIndex=-1;
+    //size_t MAxReclaimableSize=0;
+    int64_t BiggestUsableIndex=-1;
+    size_t  BiggestUsableSize = 0;
     for(size_t i = 0; i < limine_memmap_request.response->entry_count; ++i) {
+#ifdef BUKO_PRINT_MEM_INFO
         if(i < 10)  stdTerminal::printf(display, "%d ", i);
         else stdTerminal::printf(display, "%d", i);
-        stdTerminal::printf(display, "> base: %p length: %l type: (%s)\n",limine_memmap_request.response->entries[i]->base, (int64_t) limine_memmap_request.response->entries[i]->length, limine_memmap_type_to_string(limine_memmap_request.response->entries[i]->type));
+        stdTerminal::printf(display, "> base: %p length: %l pages type: (%s)\n",limine_memmap_request.response->entries[i]->base, (int64_t)(limine_memmap_request.response->entries[i]->length/4096), limine_memmap_type_to_string(limine_memmap_request.response->entries[i]->type));
+#endif
+        switch(limine_memmap_request.response->entries[i]->type) {
+                case LIMINE_MEMMAP_ACPI_RECLAIMABLE:
+                case LIMINE_MEMMAP_BOOTLOADER_RECLAIMABLE:
+                        MaxReclaimableIndex=i;
+                        break;
+                case LIMINE_MEMMAP_USABLE:
+                        if(BiggestUsableSize<limine_memmap_request.response->entries[i]->length) {
+                           BiggestUsableSize=limine_memmap_request.response->entries[i]->length;
+                           BiggestUsableIndex=i;
+                        }
+                        break;
+                default:
+                        break;
+        }
         if(limine_memmap_request.response->entries[i]->type==LIMINE_MEMMAP_USABLE) AvailableMemory+=limine_memmap_request.response->entries[i]->length;
         totalMemory+=limine_memmap_request.response->entries[i]->length;
+    }
+    if(MaxReclaimableIndex==-1) {
+        stdTerminal::printf(display, "Warning: Could not find ANY Bootloader/ACPI reclaimable memory entry");
+        for(;;) asm volatile("hlt");
     } 
+    if(BiggestUsableIndex==-1) {
+        stdTerminal::printf(display, "Warning: Could not find ANY Usable memory entry");
+        for(;;) asm volatile("hlt" : "+g"(running));
+    } 
+
+
+
+#ifdef BUKO_PRINT_MEM_INFO
     stdTerminal::printf(display, "Total available memory: %lb %lKib %lMb\n", AvailableMemory, AvailableMemory/1024, AvailableMemory/(1024*1000));
-    #ifdef BUKO_PRINT_MEM_INFO
     for(size_t i = 0; i < 9; i++) {
         putC(display,'x', limine_memmap_type_to_color(i)); stdTerminal::printf(display, " - %s\n",limine_memmap_type_to_string(i));
     }
     putC(display, '<');
     for(size_t i = 0; i < limine_memmap_request.response->entry_count; ++i) {
         double ratio = ((double)limine_memmap_request.response->entries[i]->length/(double)totalMemory);
-        //stdTerminal::printf(display, "%l> ratio*112.0 %l: ",i,(int64_t)(ratio*112.0+1));
         for(size_t j=0; j < (size_t)(ratio*MEMORY_DISPLAY_SIZE+1); ++j) {
             putStr(display, "=", limine_memmap_type_to_color(limine_memmap_request.response->entries[i]->type));
         }
-        //putC(display, '\n');
-        //break;
     }
     putC(display, '>');
     putC(display,'\n');
-    #endif
-    /*
-    char buf[12]={0};
-    itostr(buf, 12, 69);
-    putStr(display, buf);
-    */
+#endif
+    auto mapentry = limine_memmap_request.response->entries[BiggestUsableIndex];
+    size_t TotalAmountOfPages   = totalMemory/4096;
+    size_t MemMapByteSize       = TotalAmountOfPages/8;
+    if(MemMapByteSize==0) {
+        stdTerminal::printf(display, "ERROR: Cannot allocate MemMapByteSize of 0\n");
+        for(;;) asm volatile("hlt" : "+g"(running));
+    } 
+    size_t MemMapPageAllocCount = (MemMapByteSize-1)/4096+1;
+    memset((void*)mapentry->base, 0, MemMapPageAllocCount*4096);
+#ifdef BUKO_PRINT_MEM_INFO
+    stdTerminal::printf(display, "Allocating memory map at: %p with size of: %l bytes (%l pages, real size(%l))\n",mapentry->base,MemMapPageAllocCount*4096, MemMapPageAllocCount,MemMapByteSize);
+#endif
+    for(size_t i=0; i < limine_memmap_request.response->entry_count; ++i) {
+        auto entry = limine_memmap_request.response->entries[i];
+        size_t entryPageCount = entry->length/4096;
+        if(entry->type == LIMINE_MEMMAP_USABLE) { 
+            size_t PageIndex=(entry->base/4096);
+            //stdTerminal::printf(display,"Got to usable. Starting at page index: %l, length: %l\n", PageIndex, entryPageCount);
+            for(size_t j =((i==(size_t)BiggestUsableIndex) ? MemMapPageAllocCount : 0); j < entryPageCount; ++j) {
+                char* pageSet = &((char*)mapentry->base)[(PageIndex+j)/8];
+                *pageSet |= (1 << (PageIndex+j)%8);
+            }
+        }
+    }
+#ifdef BUKO_TEST_MEMORY_PAGE_MAP
+    bool passed=true;
+    size_t failureEntryIndex=0;
+    size_t failurePageIndex =0;
+
+    for(size_t i=0; i < limine_memmap_request.response->entry_count; ++i) {
+        if(!passed) break;
+        auto entry = limine_memmap_request.response->entries[i];
+        size_t PageStart = (entry->base / 4096);
+        size_t PageCount = entry->length/4096;
+        //stdTerminal::printf(display, "%l> Starting at %l with %l pages left\n",i,PageStart,PageCount);
+        for(size_t j=PageStart+(i==(size_t)BiggestUsableIndex ? MemMapPageAllocCount : 0); j < PageStart+PageCount; ++j) {
+           switch(entry->type) {
+           case LIMINE_MEMMAP_USABLE:
+                passed = getPageStatus((void*)mapentry->base, j)==PAGE_STATUS_USABLE;
+                if(!passed) {
+                   failureEntryIndex=i;
+                   failurePageIndex=j;
+                }
+                break;
+           default:
+                passed = getPageStatus((void*)mapentry->base, j)==PAGE_STATUS_UNUSABLE;
+                if(!passed) {
+                   failureEntryIndex=i;
+                   failurePageIndex=j;
+                }
+           }
+        }
+    }
+    if(passed) {
+        stdTerminal::printf(display,"Success! Passed verification check!\n");
+    } else {
+        stdTerminal::printf(display,"ERROR: Failed to verify check! at entry %l, page %l\n",(int64_t)failureEntryIndex,(int64_t)failurePageIndex);
+    }
+#endif
     putStr(display, "> ");
-    //stdTerminal::printf(display, "Hello, Blah blah: %s","World");
-    //char Buffer[500]={0};
-    //stdTerminal::sprintf(Buffer,500,"Hello, Blah blah: %s", "World");
-    //putStr(display,Buffer);
     while(running) {
         asm volatile("hlt" : "+g"(running));
     }
